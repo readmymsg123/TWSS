0b2992028d90fddadc2fbe4990eec212126013ed
Alex Wang <readmymsg123@gmail.com>
2011-01-31 01:43:50 -0500

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twss::Application.config.secret_token = '4eb18665fe501622d733ab9db6094261115c1380559a79887cccb0b7a2018e4a2bd98dc34727216f557a028c7008354af53f1d0cc0b37d3981b8b2b68f26c24c'
